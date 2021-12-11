e981fcb28d66311bb52a7aa44a025b750986db16
Benjamin Schnitzler <benjamin.schnitzler+git@googlemail.com>
2021-12-11 08:47:27 +0100

    further cleanup, grid is now an element

--- lib/tkwrapper.rb
+++ lib/tkwrapper.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
-require_relative 'grid/grid'
+require_relative 'elements/grid'
 require_relative 'elements/root'
 require_relative 'elements/frame'
 require_relative 'elements/label'
 require_relative 'elements/menu'
 require_relative 'elements/text'
+require_relative 'elements/entry'
 require_relative 'elements/auto_resize_entry'
